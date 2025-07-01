075d601c74f85b239036354a4f418a6cda7ae481
Max Spier <68516760+Mixmix00@users.noreply.github.com>
2025-07-01 14:32:40 -0700

    create main scouting form for 2025 reefscape

--- main.py
+++ main.py
@@ -1,1 +1,47 @@
 # Main program code
+import os
+import openai
+from flask import Flask, jsonify, redirect, render_template, request, url_for
+import mysql.connector
+
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='static',
+            template_folder='templates')
+
+#TODO: change these
+#connection = mysql.connector.connect(
+    #host="HOST",
+    #user="doadmin",
+    #password="PW",
+    #database="defaultdb",
+    #port=0000
+#)
+
+
+#db_cursor = connection.cursor()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/scout', methods=['GET', 'POST'])
+def scout():
+    if request.method == 'POST':
+        # TODO: Handle form submission or other POST logic here
+        return jsonify(request.form)
+    else:
+        return render_template('scoutLanding.html')
+
+@app.route('/analyze')
+def analyze():
+    return render_template('analyzeLanding.html')
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=9000)
